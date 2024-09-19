c4afd58b9178ff0083b34903c195dc28c3bfedf2
Adam Wheatley <wheatley.adam@me.com>
2024-09-18 20:38:09 -0500

    allow adding/editing if friend was created by current user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_13_022653) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_16_021042) do
   create_table "friends", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -18,6 +18,8 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_13_022653) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_friends_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
